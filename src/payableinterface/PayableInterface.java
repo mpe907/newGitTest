743d7b42b7d735bf6518f75e28fd64d9f7ac5e1e
Mark <Mark@Mark-PC.home>
2015-06-14 21:26:30 -0400

    new commit

--- src/payableinterface/PayableInterface.java
+++ src/payableinterface/PayableInterface.java
@@ -18,7 +18,7 @@ public class PayableInterface
       payableObjects[ 0 ] = new Invoice( "01234", "seat", 2, 375.00 );
       payableObjects[ 1 ] = new Invoice( "56789", "tire", 4, 79.95 );
       payableObjects[ 2 ] =
-         new SalariedEmployee( "John", "Smith", "111-11-1111", 800.00 );
+         new SalariedEmployee( "Kevin", "Smith", "111-11-1111", 800.00 );
       payableObjects[ 3 ] =
          new HourlyEmployee( "Karen", "Price", "222-22-2222", 16.75, 40 );
       payableObjects[ 4 ] =

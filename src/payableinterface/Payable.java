1ded4744069a3f1b7192a499ab68525976d151dd
Mark <Mark@Mark-PC.home>
2015-06-14 21:18:23 -0400

    new commit

---
+++ src/payableinterface/Payable.java
@@ -0,0 +1,13 @@
+/*
+Author: Mark Eife
+Java Applications: HW4 problem 10.11
+Email: meife@camden.rutgers.edu
+Payable interface declaration
+*/
+
+package payableinterface;
+
+public interface Payable 
+{
+   double getPaymentAmount(); // calculate payment; no implementation    
+} // end interface Payable
